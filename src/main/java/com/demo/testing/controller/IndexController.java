2fba144494cafa7ab54cd52e3ea28bf6fbea038c
JG Yun <nexweb1@gmail.com>
2024-01-29 08:36:31 +0900

    first commit

---
+++ src/main/java/com/demo/testing/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.demo.testing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/demo/testing/dto/ResponseDto.java
@@ -0,0 +1,50 @@
+package com.demo.testing.dto;
+
+import com.demo.testing.exception.error.MessageType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseDto<T> {
+
+    private String result = "0000";  // result code
+    private String message = "";      //
+    private T data;
+
+    public ResponseDto() {
+        this.result = MessageType.OK.toString();
+        this.message = MessageType.OK.getMessage();
+        this.data = null;
+    }
+
+    public ResponseDto( MessageType result ) {
+        this.result = result.toString();
+        this.message = result.getMessage();
+        this.data = null;
+    }
+
+    public ResponseDto( MessageType result, String message) {
+        this.result = result.toString();
+        this.message = message;
+        this.data = null;
+    }
+
+    public ResponseDto( MessageType result, T data) {
+        this.result = result.toString();
+        this.message = result.getMessage();;
+        this.data = data;
+    }
+
+    public ResponseDto( MessageType result,  String message, T data) {
+        this.result = result.toString();
+        this.message = message;
+        this.data = data;
+    }
+
+    public String toString() {
+        return "ApiResponseVO [result=" + this.result + ", resultMessage=" + this.message + ", data=" + this.data + "]";
+    }
+
+
+}
